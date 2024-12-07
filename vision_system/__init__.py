1a44cf168660fd81f5f9ed06571044e3038000d2
Duong Van Tan <tan.dv@vnu.edu.vn>
2024-12-07 12:03:38 +0700

    modify the init file in vu_vision

--- vision_system/__init__.py
+++ vision_system/__init__.py
@@ -1,1 +1,4 @@
 from .load_data import readData
+
+# from .vision_module import load_data, filtering
+# from .path_planning import archimedes_spiral
--- vision_system/vision_module-DESKTOP-U7TOHM2.py
+++
@@ -1,7 +0,0 @@
-def load_data(path):
-    # load data in here
-    pass
-
-def filtering(img):
-    # do some task with image
-    pass
